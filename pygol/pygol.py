5f59d5de526aab79438fb9fcf63c3f6aa5da0d0a
Jonatan Poveda <jon.poveda@gmail.com>
2020-04-24 11:56:13 +0200

    Add a basic game board

---
+++ pygol/pygol.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+
+import numpy as np
+import pygame
+
+# Board configuration
+height, width = 1024, 1024
+num_cells_x, num_cells_y = 25, 25
+cell_height = height / num_cells_y
+cell_width = width / num_cells_x
+
+# Colour definitions
+background_colour = 25, 25, 25
+dead_colour = 128, 128, 128
+alive_colour = 255, 255, 255
+
+# Cell status
+dead, alive = 0, 1
+
+
+def neighbourhood(game_state, x, y):
+    """ Get the Moore neighborhood (8-neighbourhood) considering a Toroidal space
+
+    Ref: https://en.wikipedia.org/wiki/Cellular_automaton
+    """
+    return (
+        game_state[(x - 1) % num_cells_x, (y - 1) % num_cells_y],
+        game_state[x % num_cells_x, (y - 1) % num_cells_y],
+        game_state[(x + 1) % num_cells_x, (y - 1) % num_cells_y],
+        game_state[(x - 1) % num_cells_x, y % num_cells_y],
+        game_state[(x + 1) % num_cells_x, y % num_cells_y],
+        game_state[(x - 1) % num_cells_x, (y + 1) % num_cells_y],
+        game_state[x % num_cells_x, (y + 1) % num_cells_y],
+        game_state[(x + 1) % num_cells_x, (y + 1) % num_cells_y],
+    )
+
+
+def update_cell(game_state, x, y):
+    """ Apply Conway's Game of Life rules
+
+    Rules:
+        1. Any live cell with two or three live neighbors survives.
+        2. Any dead cell with three live neighbors becomes a live cell.
+        3. All other live cells die in the next generation. Similarly, all other dead
+            cells stay dead.
+    """
+    n_neigh_alive = sum(neighbourhood(game_state, x, y))
+    cell_state = game_state[x, y]
+    cell_state_next = cell_state
+
+    # Any dead cell with three live neighbors becomes a live cell.
+    if cell_state == dead:
+        if n_neigh_alive == 3:
+            cell_state_next = alive
+
+    # Any live cell with two or three live neighbors survives.
+    elif cell_state == alive:
+        # Underpopulation (death by solitude)
+        if n_neigh_alive < 2:
+            cell_state_next = dead
+
+        # Overpopulation (death by starvation)
+        elif n_neigh_alive > 3:
+            cell_state_next = dead
+
+    return cell_state_next
+
+
+def update_screen(screen, game_state, x, y):
+    """ Update screen """
+    cell_status = game_state[x, y]
+    polygon = [
+        (x * cell_width, y * cell_height),
+        ((x + 1) * cell_width, y * cell_height),
+        ((x + 1) * cell_width, (y + 1) * cell_height),
+        (x * cell_width, (y + 1) * cell_height),
+    ]
+
+    if cell_status == dead:
+        pygame.draw.polygon(screen, dead_colour, polygon, width=1)
+    else:
+        pygame.draw.polygon(screen, alive_colour, polygon, width=0)
+
+
+def run(screen):
+    screen.fill(background_colour)
+    game_state = np.random.randint(0, 2, (num_cells_x, num_cells_y))
+
+    while True:
+        new_game_state = np.copy(game_state)
+        screen.fill(background_colour)
+
+        # Update all cells
+        for y in range(0, num_cells_x):
+            for x in range(0, num_cells_y):
+                new_game_state[x, y] = update_cell(game_state, x, y)
+                update_screen(screen, game_state, x, y)
+
+        # Update game state
+        game_state = np.copy(new_game_state)
+        pygame.display.flip()
+        sleep(0.1)
+
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((height, width))
+    run(screen)
